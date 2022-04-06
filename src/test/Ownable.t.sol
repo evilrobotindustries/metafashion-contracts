069eac0b21a12d2dfa7f01277080e4dca9046efe
Frank Bell <fb@evilrobot.net>
2022-04-06 17:03:57 +0100

    forge install: openzeppelin-test-helpers
    v0.5.15

---
+++ src/test/Ownable.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import "../../lib/ds-test/src/test.sol";
+import "../Contract.sol";
+
+interface CheatCodes {
+    // Expects an error on next call
+    function expectRevert() external;
+    function expectRevert(bytes calldata) external;
+    function expectRevert(bytes4) external;
+
+    // Sets the *next* call's msg.sender to be the input address
+    function prank(address) external;
+}
+
+contract OwnableTests is DSTest {
+    MetaFashion _contract;
+    CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
+
+    function setUp() public {
+        _contract = new MetaFashion();
+    }
+
+    function testPausedInitially() public {
+        assertTrue(_contract.paused());
+    }
+
+    function testUnpauses() public {
+        assertTrue(_contract.paused());
+        _contract.unpause();
+        assertTrue(!_contract.paused());
+    }
+
+    function testCannotPauseWhenAlreadyPaused() public {
+        assertTrue(_contract.paused());
+        cheats.expectRevert(abi.encodeWithSignature("Panic(uint256)", 0x11));
+        _contract.pause();
+    }
+
+    function testFailPauseAsNotOwner() public {
+        cheats.prank(address(0));
+        _contract.pause();
+    }
+
+    function testFailUnpauseAsNotOwner() public {
+        cheats.prank(address(0));
+        _contract.unpause();
+    }
+}
